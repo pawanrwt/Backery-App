73e661895d944a858003ba0a2522e630a6db90ce
Pawan Rawat <167201905+pawanrwt@users.noreply.github.com>
2024-05-31 20:52:07 +0530

    App-Start.py

--- Backery-main/main-data/App-Start.py
+++ Backery-main/main-data/App-Start.py
@@ -3,6 +3,7 @@ import App_Home as h
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
+        self.state("zoomed")
         self.attributes('-fullscreen',True)
         self.title("BACKRY SYSTEM")
         self.hp = h.Start(self.master)
